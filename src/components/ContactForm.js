aa766192de51ef748333bdd5042381745b606db7
devmikev <dev.mikev@gmail.com>
2020-09-29 18:02:10 -0400

    add image upload to firebase

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import firebase from "../components/firebase";
+import firebase, { storage } from "../components/firebase";
 
 function ContactForm() {
   const [contact, setContact] = useState({
@@ -12,12 +12,62 @@ function ContactForm() {
 
   const { name, company, email, phone, categories } = contact;
 
+  const [image, setImage] = useState();
+  const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
+  const [progress, setProgress] = useState(0);
+
   function onChange(e) {
     setContact({
       ...contact,
       [e.target.name]: e.target.value,
     });
   }
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileType = file["type"];
+      const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
+      if (validImageTypes.includes(fileType)) {
+        setError("");
+        setImage(file);
+      } else {
+        setError("Please select an image to upload");
+      }
+    }
+  };
+
+  const handleSaveImage = () => {
+    if (image) {
+      const uploadTask = storage.ref(`images/${image.name}`).put(image);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        (error) => {
+          setError(error);
+        },
+        () => {
+          storage
+            .ref("images")
+            .child(image.name)
+            .getDownloadURL()
+            .then((url) => {
+              setUrl(url);
+              setProgress(0);
+            });
+        }
+      );
+    } else {
+      setError("Error please choose an image to upload");
+    }
+  };
 
   function onSubmit(e) {
     e.preventDefault();
@@ -31,6 +81,9 @@ function ContactForm() {
         email,
         phone,
         categories,
+      })
+      .then(() => {
+        handleSaveImage();
       })
       .finally(() => {
         setContact({
@@ -47,6 +100,16 @@ function ContactForm() {
     <div>
       <form onSubmit={onSubmit}>
         <h4>Add New Contact</h4>
+        <label>Image Upload</label>
+        <div style={{ height: "10px" }}>
+          {progress > 0 ? <progress value={progress} max="100" /> : ""}
+          <p style={{ color: "red" }}>{error}</p>
+        </div>
+        <input
+          type="file"
+          accept="capture=camera"
+          onChange={handleImageChange}
+        />
         <label htmlFor="name">Name</label>
         <input onChange={onChange} type="text" name="name" id="" />
         <label htmlFor="company">Company</label>
